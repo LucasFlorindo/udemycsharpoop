f5be906e9269d2ba141a644526b2e2a656f08207
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-03-24 10:01:35 -0300

    my first commit

---
+++ aula01classatribmethstatic/aula01classatribmethstatic/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula01classatribmethstatic
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        // void = vazio, para quando a operação não emite saída, apenas altera um valor de atributo.
+        public void AdicionarProdutos (int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+            
+
+        }
+
+    }
+}
---
+++ aula01classatribmethstatic/aula01classatribmethstatic/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula01classatribmethstatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Invoca classe produto numa variável p, e atribuindo um valor new quando invocado nessa chave Main
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade em estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a serem adicionados:");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a serem removidos:");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+
+
+
+        }
+    }
+}
