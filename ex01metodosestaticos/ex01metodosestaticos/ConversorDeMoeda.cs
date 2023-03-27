8a180282c57558d765bf9dc6e8eb492ec8f8cc5d
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-03-27 09:44:57 -0300

    membros estáticos

---
+++ ex01metodosestaticos/ex01metodosestaticos/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01metodosestaticos
+{
+    class ConversorDeMoeda
+    {
+
+        public static double Iof = 6.0;
+        public static double PrecoReal(double qtd,double dolar)
+        {
+            double total = qtd * dolar;
+            return total + total * Iof / 100;
+        }
+    }
+}
---
+++ ex01metodosestaticos/ex01metodosestaticos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ex01metodosestaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            double result = ConversorDeMoeda.PrecoReal(qtd, dolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+          
+        }
+    }
+}
