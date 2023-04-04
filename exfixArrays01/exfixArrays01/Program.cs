d5609d894d31109f9fa6b0ee3566088f6d52ad34
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-04 10:49:53 -0300

    Aula C# udemy

---
+++ exfixArrays01/exfixArrays01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exfixArrays01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+
+                Console.Write("Nome:");
+                string nome = Console.ReadLine();
+
+
+                Console.Write("Email:");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto:");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i<0; i++)
+            {
+                if (vect[i] != null){
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+
+        }
+    }
+}
