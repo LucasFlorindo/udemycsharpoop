fe94b59a0c48aa431d069efdcacd9c501b7c83c7
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-04 11:24:22 -0300

    Aula C# udemy

---
+++ aulaParams/aulaParams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aulaParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Sum(new int[] { 10, 20, 30, 40 });
+
+            // ref faz a nova variável apontar para o mesmo escopo da variavel referenciada ex: int a = 10; ref int x  = onde a aponta.
+
+        }
+    }
+}
