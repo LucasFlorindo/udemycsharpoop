885b51a26002ce08cf6aac1bbea59f90cf4503e7
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-04 09:46:39 -0300

    Aulas c# udemy

---
+++ aulaArrays02/aulaArrays02/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aulaArrays02
+{
+    class Product
+    {
+        // declarar propriedades  "prop tab tab"
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
