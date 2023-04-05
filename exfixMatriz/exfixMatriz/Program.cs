95791718ae33acf3463b25360b70f11de624289d
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-05 11:44:54 -0300

    Sintaxe alternativa switch case

--- exfixMatriz/exfixMatriz/Program.cs
+++ exfixMatriz/exfixMatriz/Program.cs
@@ -26,7 +26,7 @@ namespace Course
 
             for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < n; j++) 
                 {
                     if (mat[i, j] == x)
                     {
---
+++ switchcaseIf/switchcaseIf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcaseIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+
+            //double desconto;
+
+            // if (preco < 20)
+            // {
+            //     desconto = preco * 0.1;
+            // }
+            // else
+            // {
+            //     desconto = preco * 0.05;
+            //  }
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
