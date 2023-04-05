6f12044e2a8a58d2193e8b5d0757157fb9e210e3
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-05 10:43:39 -0300

    Exercício de fixação matriz

---
+++ exfixMatriz/exfixMatriz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exfixMatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split(' ');
+            int n = int.Parse(line[0]);
+            int m = int.Parse(line[1]);
+
+            int[,] mat = new int[m, n];
+
+            for (int i = 0; i<m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j<n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+
+        }
+    }
+}
