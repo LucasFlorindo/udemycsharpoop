9fd6870426f2705e0baa7934963c1337656fcd70
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-05 10:15:34 -0300

    Exercício de matrizes

---
+++ exResolvidoMat/exResolvidoMat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exResolvidoMat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //matriz de ordem N = matriz quadratica
+
+            //Programa recebe a construção da amtriz pelo usuário, ex "3"
+            // Programa recebe os dados da matriz pelo usuário
+            //  Programa recebe os dados da diagonal e mostra quantos números negativos aparecem.
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+
+
+            }
+
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Negative numbers: " + count);
+        }
+
+
+
+    }
+}
