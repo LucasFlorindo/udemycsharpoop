17d74df754454ec9bfbd30b8ad5a4d2104097d31
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-05 11:11:08 -0300

    Exercicio fixação matrizes

--- exfixMatriz/exfixMatriz/Program.cs
+++ exfixMatriz/exfixMatriz/Program.cs
@@ -1,30 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace exfixMatriz
+namespace Course
 {
     class Program
     {
         static void Main(string[] args)
         {
+
             string[] line = Console.ReadLine().Split(' ');
-            int n = int.Parse(line[0]);
-            int m = int.Parse(line[1]);
+            int m = int.Parse(line[0]);
+            int n = int.Parse(line[1]);
 
             int[,] mat = new int[m, n];
 
-            for (int i = 0; i<m; i++)
+            for (int i = 0; i < m; i++)
             {
                 string[] values = Console.ReadLine().Split(' ');
-                for (int j = 0; j<n; j++)
+                for (int j = 0; j < n; j++)
                 {
                     mat[i, j] = int.Parse(values[j]);
                 }
             }
 
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < n - 1)
+                        {
+                            Console.WriteLine("Right: " + mat[i, j + 1]);
+                        }
+                        if (i < m - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+
+
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
