95d4feec6a403c0f536c3ae0bfe6f7400ec6cf8c
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-06 13:54:52 -0300

    Funcoes string

--- aulafuncint/aulafuncint/Program.cs
+++ aulafuncint/aulafuncint/Program.cs
@@ -8,7 +8,7 @@ namespace aulafuncint
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)///
         {
             string original = "abcde FGHIJ ABC abc DEFG   ";
 
@@ -16,12 +16,20 @@ namespace aulafuncint
             string s2 = original.ToLower();
             string s3 = original.Trim();
             int n1 = original.IndexOf("bc");
+            int n2 = original.LastIndexOf("bc");
+
+            string s4 = original.Substring(3);
+            string s5 = original.Replace();
 
             Console.WriteLine("Original: " + original);
             Console.WriteLine("Original com letras maiúsculas(ToUpper): " + s1);
             Console.WriteLine("Original com letras minúsculas(ToLower): " + s2);
             Console.WriteLine("Original removendo espaços em excesso(trim): " + s3);
             Console.WriteLine("IndexOf('bc'): " + n1);
+            Console.WriteLine("LasrIndexOf('bc'): " + n2);
+            Console.WriteLine("Substring: " + s4);
+            Console.WriteLine("");
+
 
 
 
