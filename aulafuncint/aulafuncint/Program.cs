cb14d51cd5d6270840c446d692023ca616d3145f
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-06 12:48:57 -0300

    funcoes string

---
+++ aulafuncint/aulafuncint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aulafuncint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string original = "abcde FGHIJ ABC abc DEFG   ";
+
+            string s1 = original.ToUpper();
+
+            Console.WriteLine("Original: " + original);
+            Console.WriteLine("Original com letras maiúsculas: "+ s1);
+        }
+    }
+}
