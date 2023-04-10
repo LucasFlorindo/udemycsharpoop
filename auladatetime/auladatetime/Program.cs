364db0a4a681b90fd39fae0a9f70ee7a60d216c8
Lucas Florindo <114751354+LucasFlorindo@users.noreply.github.com>
2023-04-10 09:32:15 -0300

    DateTime

---
+++ auladatetime/auladatetime/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auladatetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2023, 04, 10, 09, 25, 00);
+            Console.WriteLine(d2);
+
+            DateTime d4 = DateTime.Now;
+            DateTime d5 = DateTime.Today;
+            DateTime d6 = DateTime.UtcNow;
+
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+            Console.WriteLine(d6);
+        }
+    }
+}
